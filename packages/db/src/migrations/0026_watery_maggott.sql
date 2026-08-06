2a47e81ebda46342720b0dd4473d69ddcdfaa188
Zaccal <ksss90411@gmail.com>
2026-08-06 14:31:16 +0500

    🐛 fix: fixed migration file

--- packages/db/src/migrations/0026_watery_maggott.sql
+++ packages/db/src/migrations/0026_watery_maggott.sql
@@ -1,9 +1,18 @@
-ALTER TABLE "organizations_table"
+DO $$
+BEGIN
+    CREATE TYPE "public"."template_type" AS ENUM ('HOTEL', 'APARTMENT');
+EXCEPTION
+    WHEN duplicate_object THEN NULL;
+END
+$$;
+--> statement-breakpoint
+
+ALTER TABLE "public"."organizations_table"
 ADD COLUMN IF NOT EXISTS "template_type"
 "public"."template_type"
 DEFAULT 'APARTMENT'
 NOT NULL;
 --> statement-breakpoint
 
-ALTER TABLE "documents_table"
+ALTER TABLE "public"."documents_table"
 DROP COLUMN IF EXISTS "template_type";
